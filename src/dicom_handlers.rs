e08d7459b7a1298f6b59d462d86541cad296298d
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-02-25 22:42:24 +0100

    DicomDumper impl just for dumping dicom tags to screen

--- src/dicom_handlers.rs
+++ src/dicom_handlers.rs
@@ -29,3 +29,35 @@ impl abstractions::DicomHandler for DicomContainer {
         child_index
     }
 }
+
+pub struct DicomDumper {
+    tags_count: usize
+}
+
+impl DicomDumper {
+    pub fn new() -> DicomDumper {
+        DicomDumper { 
+            tags_count: 0
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.tags_count
+    }
+}
+
+
+impl abstractions::DicomHandler for DicomDumper {
+    fn handle_tag(&mut self, _: usize, tag: DicomTag) -> usize {
+        let tag_name = match tags::try_tag_name(tag.id.0, tag.id.1) {
+            Some(name) => name, 
+            None       => "UNKNOWN"
+        };
+
+        println!("TAG | {} | ({}, {}) | {} | {}", tag.vr, tag.id.0, tag.id.1, tag_name, tag.value);
+
+        self.tags_count += 1;
+
+        self.tags_count
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod sop_class;
 mod transfer_syntax;
 mod tags;
 mod dicom_tag;
-mod dicom_container;
+mod dicom_handlers;
 mod dicom;
 mod abstractions;
 
@@ -16,13 +16,13 @@ use std::time::Instant;
 use std::fs::File;
 
 use crate::dicom_tag::*;
-use crate::dicom_container::*;
+use crate::dicom_handlers::*;
 
 const MIN_ARGUMENTS: usize = 2;
 
-fn process_dcim(dcim_file_path: &str) {
+fn load_dcim(dcim_file_path: &str) {
 
-    println!("PROCESSING {} ...", dcim_file_path);
+    println!("LOADING DICOM TAGS IN {} ...", dcim_file_path);
 
     let root = Node { tag: DicomTag::empty(), children: Vec::new() };
 
@@ -36,6 +36,20 @@ fn process_dcim(dcim_file_path: &str) {
     println!("Found {} dicom nodes", container.nodes.len());
 }
 
+fn dump_dcim(dcim_file_path: &str) {
+
+    println!("DUMPING DICOM TAGS IN {} ...", dcim_file_path);
+
+    let mut container = DicomDumper::new();
+
+    match File::open(dcim_file_path) {
+        Ok(mut reader) => dicom::parse(&mut reader, &mut container),
+        Err(err)   => println!("ERROR: COULD NOT LOAD {}. {}", dcim_file_path, err)
+    }
+
+    println!("Found {} dicom nodes", container.len());
+}
+
 fn main() {
     println!("DICOM COMMAND LINE PARSER");
 
@@ -44,7 +58,7 @@ fn main() {
     let now = Instant::now();    
 
     match args.len() {
-        MIN_ARGUMENTS => process_dcim(&args[1]),
+        MIN_ARGUMENTS => dump_dcim(&args[1]),
         _             => println!("ERROR: UNEXPECTED NUMBER OF ARGUMENTS")
     }
 
